669f7cd80ce77036c82887ee273fa2193973e8b3
Hafsa-Belahnech <250507787+Hafsa-Belahnech@users.noreply.github.com>
2026-03-17 21:38:21 +0000

    fix

--- state/src/MessageBouton.jsx
+++ state/src/MessageBouton.jsx
@@ -3,6 +3,7 @@ import { useState } from 'react';
 function MessageBouton() {
   const [message, setMessage] = useState('Cliquez sur le bouton');
 
+  //evenement onClick (par clique sur le bouton)
   function changerMessage() {
     setMessage('Vous avez cliqué sur le bouton !');
   }
